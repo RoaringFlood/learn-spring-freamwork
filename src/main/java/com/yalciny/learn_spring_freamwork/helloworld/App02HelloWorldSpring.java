b3f0d9527adec69ba8470bb16254974b5391b74e
Yalçın <yalcinyetginoglu@gmail.com>
2025-04-03 14:26:37 +0300

    New files

--- src/main/java/com/yalciny/learn_spring_freamwork/helloworld/App02HelloWorldSpring.java
+++ src/main/java/com/yalciny/learn_spring_freamwork/helloworld/App02HelloWorldSpring.java
@@ -8,26 +8,28 @@ public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
 		//1: Launch a Spring Context
-		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+		try(var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class))
+		{
+			//2: Configure the things that we want Spring to manage - @Configuration
+			//HelloWorldConfiguration içinde @Configuration tanımladık
+			// @Bean olarakta "name" class ını ekledik
+			
+			System.out.println(context.getBean("name"));
+			System.out.printf("Age is %s\n",context.getBean("age"));
+			
+			System.out.println(context.getBean("person"));
+			System.out.println(context.getBean("person2MethodCall"));
+			System.out.println(context.getBean("person3Paramaters"));
+			System.out.println(context.getBean("person4Qualifier"));
+			
+			System.out.println(context.getBean("adres"));
+			System.out.println(context.getBean("adres2"));
+			System.out.println(context.getBean(Address.class));
+			
+			//Springin kontrol ettiği tüm bean leri listeler
+			//Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);	
+		}
 		
-		//2: Configure the things that we want Spring to manage - @Configuration
-		//HelloWorldConfiguration içinde @Configuration tanımladık
-		// @Bean olarakta "name" class ını ekledik
-		
-		System.out.println(context.getBean("name"));
-		System.out.printf("Age is %s\n",context.getBean("age"));
-		
-		System.out.println(context.getBean("person"));
-		System.out.println(context.getBean("person2MethodCall"));
-		System.out.println(context.getBean("person3Paramaters"));
-		System.out.println(context.getBean("person4Qualifier"));
-		
-		System.out.println(context.getBean("adres"));
-		System.out.println(context.getBean("adres2"));
-		System.out.println(context.getBean(Address.class));
-		
-		//Springin kontrol ettiği tüm bean leri listeler
-		//Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
 }
